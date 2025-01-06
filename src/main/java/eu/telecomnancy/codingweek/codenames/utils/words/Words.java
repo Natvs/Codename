385b8d2ab2822bcf3331e2b6749f6e1bfa120006
SIMONNEAU Lucas <lucas.simonneau@telecomnancy.eu>
2025-01-06 15:57:48 +0100

    feat(words): add a function which create a list of words which will be used in the grid. Add tests. Still don't implements the type of each word (of which team it belongs)

---
+++ src/main/java/eu/telecomnancy/codingweek/codenames/utils/words/Words.java
@@ -0,0 +1,11 @@
+package eu.telecomnancy.codingweek.codenames.utils.words;
+
+public class Words {
+    private String name;
+    public Words(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+}
