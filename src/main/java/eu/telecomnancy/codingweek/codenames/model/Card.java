024f5fcd06dcbb5ee697490c5f713809aed9d47c
Arthur Jeanne <arthur.jeanne@telecomnancy.eu>
2025-01-06 17:37:01 +0100

    feat(model): currently dosn't work but model's class went well

---
+++ src/main/java/eu/telecomnancy/codingweek/codenames/model/Card.java
@@ -0,0 +1,47 @@
+package eu.telecomnancy.codingweek.codenames.model;
+
+public class Card {
+    
+    private String name;
+    private Color color;
+    private Boolean revealed;
+
+    public Card(String name, Color color) {
+        this.name = name;
+        this.color = color;
+        this.revealed = false;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Color getColor() {
+        return this.color;
+    }
+
+    public Boolean getRevealed() {
+        return this.revealed;
+    }
+
+    public void show() {
+        this.revealed = true;
+    }
+
+    public CardState getState(Color color) {
+        if (color == this.color) {
+            return CardState.GOOD;
+        } else {
+            if (color == Color.BLACK) {
+                return CardState.FORBIDDEN;
+            } else {
+                if (color == Color.WHITE) {
+                    return CardState.NEUTRAL;
+                } else {
+                    return CardState.ENEMY;
+                }
+            }
+        }
+    }
+
+}
--- src/main/java/eu/telecomnancy/codingweek/codenames/model/Team.java
+++ src/main/java/eu/telecomnancy/codingweek/codenames/model/Team.java
@@ -5,10 +5,22 @@ import java.util.List;
 
 public class Team {
     
+    private Color color;
     private List<Player> playersList = new ArrayList<>();
+    private Session session;
 
-    public Team(List<Player> playersList) {
+    public Team(Color color, List<Player> playersList, Session session) {
+        if ((color == Color.BLACK) || (color == Color.WHITE)) {
+            this.color = color;
+        } else {
+            this.color = null;
+        }
         this.playersList = playersList;
+        this.session = session;
+    }
+
+    public Color getColor() {
+        return this.color;
     }
 
     public List<Player> getPlayersList() {
