e6fa5ea30da51e0317e6ff210b75024bd3ca7645
Luca Mandrelli <73400538+lmandrelli@users.noreply.github.com>
2025-01-06 16:52:18 +0100

    feat(styling) : minimal styling and placement

--- src/main/java/eu/telecomnancy/codingweek/codenames/controller/NewConfigController.java
+++ src/main/java/eu/telecomnancy/codingweek/codenames/controller/NewConfigController.java
@@ -1,5 +1,10 @@
 package eu.telecomnancy.codingweek.codenames.controller;
 
+import javafx.fxml.FXML;
+
 public class NewConfigController {
-    
+    @FXML
+    private void onBack() {
+        RootController.getInstance().changeView("/views/home.fxml");
+    }
 }
