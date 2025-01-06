bf8999dfbb01a7bf2e488089823356c57f5bdcd3
Luca Mandrelli <luca.mandrelli@telecomnancy.eu>
2025-01-06 17:28:38 +0100

    feat(newConfig): config page

--- src/main/java/eu/telecomnancy/codingweek/codenames/controller/NewConfigController.java
+++ src/main/java/eu/telecomnancy/codingweek/codenames/controller/NewConfigController.java
@@ -1,10 +1,34 @@
 package eu.telecomnancy.codingweek.codenames.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 
 public class NewConfigController {
     @FXML
+    private Button startButton;
+
+    @FXML
+    private ComboBox<String> thematicSelection;
+
+    @FXML
+    private void initialize() {
+        startButton.setDisable(true);
+        thematicSelection.getItems().addAll("Tout", "Patate", "Entropie");
+    }
+
+    @FXML
     private void onBack() {
         RootController.getInstance().changeView("/views/home.fxml");
+    }
+
+    @FXML
+    private void onStart() {
+        RootController.getInstance().changeView("/views/game.fxml");
+    }
+
+    @FXML
+    private void onThematicSelection() {
+        startButton.setDisable(false);
     }
 }
