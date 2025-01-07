ad1ad1683b723eb5f52cd5efe49cf9b6d907e19b
Nathan Vernois <nathan.vernois@telecomnancy.eu>
2025-01-07 09:04:23 +0100

    feat(shortcuts): added shortcuts for config window

--- src/main/java/eu/telecomnancy/codingweek/codenames/controller/NewConfigController.java
+++ src/main/java/eu/telecomnancy/codingweek/codenames/controller/NewConfigController.java
@@ -3,8 +3,16 @@ package eu.telecomnancy.codingweek.codenames.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.GridPane;
+
+import java.awt.event.KeyEvent;
+import java.beans.EventHandler;
 
 public class NewConfigController {
+
+    @FXML
+    private GridPane mainPane;
     @FXML
     private Button startButton;
 
@@ -13,6 +21,17 @@ public class NewConfigController {
 
     @FXML
     private void initialize() {
+        mainPane.setOnKeyPressed((keyevent) -> {
+            System.out.println("Key pressed : " + keyevent.getCode());
+            switch (keyevent.getCode()) {
+                case KeyCode.Q:
+                    RootController.getInstance().changeView("/views/home.fxml");
+                    break;
+                case KeyCode.S:
+                    RootController.getInstance().changeView("/views/game.fxml");
+                    break;
+            }
+        });
         startButton.setDisable(true);
         thematicSelection.getItems().addAll("Tout", "Patate", "Entropie");
     }
@@ -31,4 +50,5 @@ public class NewConfigController {
     private void onThematicSelection() {
         startButton.setDisable(false);
     }
+
 }
