b7dca6552962a02ec4137757098a3cb24e5d6839
SIMONNEAU Lucas <lucas.simonneau@telecomnancy.eu>
2025-01-08 11:34:07 +0100

    feat(turn): add test file, pick last version from main

---
+++ src/test/java/eu/telecomnancy/codingweek/codenames/model/BoardTest.java
@@ -0,0 +1,32 @@
+package eu.telecomnancy.codingweek.codenames.model;
+
+import eu.telecomnancy.codingweek.codenames.model.board.Board;
+import eu.telecomnancy.codingweek.codenames.model.color.Color;
+import eu.telecomnancy.codingweek.codenames.model.board.Card;
+import eu.telecomnancy.codingweek.codenames.model.game.Session;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+
+import org.junit.jupiter.api.Test;
+
+public class BoardTest {
+    @Test
+    void testRemainingCards() {
+        Session session = new Session();
+        Board board = session.getBoard();
+        Card[][] grid = board.getGrid();
+        int heigth = board.getHeigth();
+        int width = board.getWidth();
+        for (int j = 0; j < heigth; j++) {
+            for (int i = 0; i < width; i++) {
+                if (grid[i][j].getColor() == Color.BLUE) {
+                    grid[i][j].show();
+                }
+            }
+        }
+        assertEquals(0, board.getRemainingCards(Color.BLUE));
+        assertNotEquals(0, board.getRemainingCards(Color.RED));
+    }
+}
