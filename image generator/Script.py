fc426cc0b044b38a5e1857d990be9543c0e7f0fd
Luca Mandrelli <luca.mandrelli@telecomnancy.eu>
2025-01-08 17:19:46 +0100

    feat(image_gen) : new script to stable diffusion 3.5 turbo

--- image generator/Script.py
+++ image generator/Script.py
@@ -1,27 +1,36 @@
-from diffusers import StableDiffusionPipeline
+from diffusers import DiffusionPipeline
 import torch
 import os
 
 if torch.cuda.is_available():
     print("Cuda est disponible !")
+elif torch.backends.mps.is_available():
+    print("MPS dispo")
+    mps_device = torch.device("mps")
+    x = torch.ones(1, device=mps_device)
+    print(x)
 else:
-    print("Cuda n'est pas disponible !")
+    print("Aucune n'est pas disponible !")
 
 
 # Fonction pour générer plusieurs images à partir d'un prompt
-def generate_images(prompt, negative_prompt="bruit, fond flou, mauvaise qualité, moche", 
+def generate_images(prompt, negative_prompt="ugly, NSFW, scary, gross, weird, creepy, bad quality", 
     output_dir="BdD", image_size=(512, 512), num_images=4):
+    
+    new_prompt = f"Represent the concept of '{prompt}', it should be detailed"
     
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
     # Charger le pipeline Stable Diffusion
-    pipeline = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4")
-    pipeline = pipeline.to("cuda" if torch.cuda.is_available() else "cpu")
+    pipeline = DiffusionPipeline.from_pretrained(
+        "stabilityai/stable-diffusion-3.5-large-turbo"
+    )
+    pipeline = pipeline.to("mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu")
 
     # Générer plusieurs images avec prompt négatif (si fourni)
     for i in range(num_images):
-        image = pipeline(prompt, negative_prompt=negative_prompt, height=image_size[0], width=image_size[1]).images[0]
+        image = pipeline(new_prompt, negative_prompt=negative_prompt, height=image_size[0], width=image_size[1]).images[0]
         
         # Sauvegarder chaque image avec un nom unique
         output_path = os.path.join(output_dir, f"{prompt.replace(' ', '_')}_{i+1}.png")
