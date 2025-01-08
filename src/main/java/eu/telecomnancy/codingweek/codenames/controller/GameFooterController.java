774dbdf605ef6dcb85778914eb3ffc4c4516e5e4
SIMONNEAU Lucas <lucas.simonneau@telecomnancy.eu>
2025-01-08 21:12:30 +0100

    fix(display-role): forgot to add some files

---
+++ src/main/java/eu/telecomnancy/codingweek/codenames/controller/GameFooterController.java
@@ -0,0 +1,39 @@
+package eu.telecomnancy.codingweek.codenames.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class GameFooterController {
+    private GameController gameController;
+    private boolean isAgent;
+    @FXML
+    private TextField word;
+    @FXML
+    private Label indication;
+
+    public GameFooterController(GameController gameController,boolean isAgent){
+        this.gameController = gameController;
+        this.isAgent = isAgent;
+    }
+
+    private void setFooter(){
+        
+    }
+
+    @FXML
+    private void onQuit(){
+        gameController.onQuit();
+    }
+    @FXML
+    private void onSubmit() {
+        if (isAgent){
+            String hintValue = word.getText();
+            System.out.println(hintValue);
+            gameController.setHint(hintValue);
+        } else {
+            indication.setText("test");
+        }
+        gameController.onSubmit();
+    }
+}
