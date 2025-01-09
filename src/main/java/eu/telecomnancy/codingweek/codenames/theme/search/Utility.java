0e0a8eecd3f81c916c851651fe423f3bd3acd442
Arthur Jeanne <arthur.jeanne@telecomnancy.eu>
2025-01-09 22:27:00 +0100

    fix(Them): the search function works

--- src/main/java/eu/telecomnancy/codingweek/codenames/theme/search/Utility.java
+++ src/main/java/eu/telecomnancy/codingweek/codenames/theme/search/Utility.java
@@ -1,5 +1,6 @@
 package eu.telecomnancy.codingweek.codenames.theme.search;
 
+import java.text.Normalizer;
 import java.util.ArrayList;
 
 import eu.telecomnancy.codingweek.codenames.theme.search.Utility;
@@ -7,13 +8,26 @@ import eu.telecomnancy.codingweek.codenames.utils.openCardsService;
 
 
 public class Utility {
+
+    // Fonction pour normaliser une chaîne (supprimer les accents et insensibilité à la casse)
+    public static String normaliseString(String text) {
+        // Remplacer la ligature "œ" par "oe" manuellement
+        text = text.replace("œ", "oe").replace("Œ", "oe");
+        // Supprimer les accents en utilisant la normalisation Unicode
+        String normalizedText = Normalizer.normalize(text, Normalizer.Form.NFD);
+        // Retirer les caractères diacritiques (accents)
+        normalizedText = normalizedText.replaceAll("\\p{M}", "").toLowerCase();
+        return normalizedText;
+    }
     
     public static ArrayList<String> getSearchDico(String keyWord) {
         ArrayList<String> dico = openCardsService.openFile("src/main/resources/words/codenames_words.txt");
         ArrayList<String> searchDico = new ArrayList<>();
 
+        String NormalisedKeyWord = normaliseString(keyWord);
         for (String word : dico) {
-            if (word.startsWith(keyWord)) {
+            String NormalisedWord = normaliseString(word);
+            if (NormalisedWord.startsWith(NormalisedKeyWord)) {
                 searchDico.add(word);
             }
         }
